f0074ff1df8fb5a204d8c496d326863c55607361
Danielwsx64 <danielws.x64@gmail.com>
2022-09-12 12:07:22 -0300

    Add Elixir pipelize undo function

--- lua/danielws/elixir/pipelize.lua
+++ lua/danielws/elixir/pipelize.lua
@@ -6,6 +6,13 @@ local function trim(str)
 	return result
 end
 
+local function has_pipe(fn_call)
+	if string.find(fn_call, "|>") then
+		return true
+	end
+	return false
+end
+
 local function get_first_arg_when_is_collection(args)
 	local open_char = string.match(args, "^%s*%%?[%.%w]*([{%[])")
 
@@ -53,8 +60,14 @@ local function split_assigning_statement(line)
 	return "", line
 end
 
+local function get_args(fn_call)
+	local fn_name, args = string.match(fn_call, "^%s*([%w_%.]+)%((.*)%)")
+
+	return fn_name, args
+end
+
 local function pipelize(fn_call)
-	local fn_name, args = string.match(fn_call, "^([%w_%.]+)%((.*)%)")
+	local fn_name, args = get_args(fn_call)
 
 	if args == "" then
 		return string.format("%s()", fn_name)
@@ -77,60 +90,105 @@ local function pipelize(fn_call)
 	return string.format("%s |> %s(%s)", first_arg, fn_name, tail_args)
 end
 
+local function add_first_arg(first_arg, fn_call)
+	local fn_name, args = get_args(fn_call)
+
+	if args ~= "" then
+		return string.format("%s(%s, %s)", fn_name, first_arg, args)
+	end
+
+	return string.format("%s(%s)", fn_name, first_arg)
+end
+
+local function undo_pipelize(line)
+	local first_arg, fn_call = string.match(line, "^(.*)|>(.*)")
+
+	if has_pipe(first_arg) then
+		return add_first_arg(undo_pipelize(first_arg), trim(fn_call))
+	end
+
+	return add_first_arg(trim(first_arg), fn_call)
+end
+
 function Self.into_pipe(line)
 	local assign_statement, fn_call = split_assigning_statement(trim(line))
 
-	return assign_statement .. pipelize(fn_call)
+	if has_pipe(fn_call) then
+		return assign_statement .. undo_pipelize(fn_call)
+	else
+		return assign_statement .. pipelize(fn_call)
+	end
 end
 
 -- Used for test
--- local function test(value, expected)
--- 	if value ~= expected then
--- 		print(string.format("Test failed\n\nExpected:\n%s\n\nGot:\n%s\n\n", expected, value))
+-- local function test(no_pipe, with_pipe, index)
+-- 	local pipe_result = Self.into_pipe(no_pipe)
+--
+-- 	if pipe_result ~= with_pipe then
+-- 		print(string.format("Test PIPELIZE failed %s\n\nExpected:\n%s\n\nGot:\n%s\n\n", index, with_pipe, pipe_result))
+-- 		return false
+-- 	end
+--
+-- 	local undo_result = Self.into_pipe(with_pipe)
+--
+-- 	if undo_result ~= trim(no_pipe) then
+-- 		print(
+-- 			string.format("Test UNDO_PIPELIZE failed %s\n\nExpected:\n%s\n\nGot:\n%s\n\n", index, no_pipe, undo_result)
+-- 		)
 -- 		return false
 -- 	end
 --
 -- 	return true
 -- end
 --
--- local primeira = "funcao(valor_one, valor_two)"
--- local segunda = "variavel = funcao(valor_one, valor_two)"
--- local terceira = "\nvariavel \n= \nfuncao(valor_one, valor_two)"
--- local quarta = "funcao(valor_one)"
--- local quinta = "funcao(outra_funcao(valor), segundo_arg)"
--- local sexta = "valor_final = funcao(outra_funcao(terceira()), segundo_arg)"
--- local setima = "final = funcao(valor, build_qualquer_coisa(%{map: valor}, &build(&1)))"
--- local oitava = 'funcao(%{"daniel" => "valor", outra: outro}, build_qualquer_coisa(&build(&1)))'
--- local nona = 'funcao(%Elixir.Struct{"daniel" => "valor", outra: outro}, build())'
--- local deca = 'funcao(%Elixir.Struct{"daniel" => "valor", outra: [1, 2]}, build())'
--- local onza = "funcao([1, 2], build())"
--- local doza = "funcao([1, %{daniel: valor}], build())"
--- local treza = "funcao({:ok,  %{ list: [1, [ 2, 3]] }, map: %{ key: value} }, build())"
--- local quator = [[
---     um_complexo =
---       print(%{
---         key: value
---       })
--- ]]
--- local quinze = "valor = mais_um(%{map: value}, {:ok, [1, 2]})"
--- local dezeis = "company = Organization.get_company(connection.company_id)"
+-- local examples = {
+-- 	{ "funcao(valor_one, valor_two)", "valor_one |> funcao(valor_two)" },
+-- 	{ "variavel = funcao(valor_one, valor_two)", "variavel = valor_one |> funcao(valor_two)" },
+-- 	{ "\nvariavel \n= \nfuncao(valor_one, valor_two)", "variavel = valor_one |> funcao(valor_two)" },
+-- 	{ "funcao(valor_one)", "valor_one |> funcao()" },
+-- 	{ "funcao(outra_funcao(valor), segundo_arg)", "valor |> outra_funcao() |> funcao(segundo_arg)" },
+-- 	{
+-- 		"valor_final = funcao(outra_funcao(terceira()), segundo_arg)",
+-- 		"valor_final = terceira() |> outra_funcao() |> funcao(segundo_arg)",
+-- 	},
+-- 	{
+-- 		"final = funcao(valor, build_qualquer_coisa(%{map: valor}, &build(&1)))",
+-- 		"final = valor |> funcao(build_qualquer_coisa(%{map: valor}, &build(&1)))",
+-- 	},
+-- 	{
+-- 		'funcao(%{"daniel" => "valor", outra: outro}, build_qualquer_coisa(&build(&1)))',
+-- 		'%{"daniel" => "valor", outra: outro} |> funcao(build_qualquer_coisa(&build(&1)))',
+-- 	},
+-- 	{
+-- 		'funcao(%Elixir.Struct{"daniel" => "valor", outra: outro}, build())',
+-- 		'%Elixir.Struct{"daniel" => "valor", outra: outro} |> funcao(build())',
+-- 	},
+-- 	{
+-- 		'funcao(%Elixir.Struct{"daniel" => "valor", outra: [1, 2]}, build())',
+-- 		'%Elixir.Struct{"daniel" => "valor", outra: [1, 2]} |> funcao(build())',
+-- 	},
+-- 	{ "funcao([1, 2], build())", "[1, 2] |> funcao(build())" },
+-- 	{ "funcao([1, %{daniel: valor}], build())", "[1, %{daniel: valor}] |> funcao(build())" },
+-- 	{
+-- 		"funcao({:ok, %{ list: [1, [ 2, 3]] }, map: %{ key: value} }, build())",
+-- 		"{:ok, %{ list: [1, [ 2, 3]] }, map: %{ key: value} } |> funcao(build())",
+-- 	},
+-- 	{ "valor = mais_um(%{map: value}, {:ok, [1, 2]})", "valor = %{map: value} |> mais_um({:ok, [1, 2]})" },
+-- 	{
+-- 		"company = Organization.get_company(connection.company_id)",
+-- 		"company = connection.company_id |> Organization.get_company()",
+-- 	},
+-- 	{ [[
+-- 		um_complexo =
+-- 			print(%{
+-- 				key: value
+-- 			})
+-- ]], "um_complexo = %{ key: value } |> print()" },
+-- }
 --
--- assert(test(Self.into_pipe(primeira), "valor_one |> funcao(valor_two)"))
--- assert(test(Self.into_pipe(segunda), "variavel = valor_one |> funcao(valor_two)"))
--- assert(test(Self.into_pipe(terceira), "variavel = valor_one |> funcao(valor_two)"))
--- assert(test(Self.into_pipe(quarta), "valor_one |> funcao()"))
--- assert(test(Self.into_pipe(quinta), "valor |> outra_funcao() |> funcao(segundo_arg)"))
--- assert(test(Self.into_pipe(sexta), "valor_final = terceira() |> outra_funcao() |> funcao(segundo_arg)"))
--- assert(test(Self.into_pipe(setima), "final = valor |> funcao(build_qualquer_coisa(%{map: valor}, &build(&1)))"))
--- assert(test(Self.into_pipe(oitava), '%{"daniel" => "valor", outra: outro} |> funcao(build_qualquer_coisa(&build(&1)))'))
--- assert(test(Self.into_pipe(nona), '%Elixir.Struct{"daniel" => "valor", outra: outro} |> funcao(build())'))
--- assert(test(Self.into_pipe(deca), '%Elixir.Struct{"daniel" => "valor", outra: [1, 2]} |> funcao(build())'))
--- assert(test(Self.into_pipe(onza), "[1, 2] |> funcao(build())"))
--- assert(test(Self.into_pipe(doza), "[1, %{daniel: valor}] |> funcao(build())"))
--- assert(test(Self.into_pipe(treza), "{:ok, %{ list: [1, [ 2, 3]] }, map: %{ key: value} } |> funcao(build())"))
--- assert(test(Self.into_pipe(quator), "um_complexo = %{ key: value } |> print()"))
--- assert(test(Self.into_pipe(quinze), "valor = %{map: value} |> mais_um({:ok, [1, 2]})"))
--- assert(test(Self.into_pipe(dezeis), "company = connection.company_id |> Organization.get_company()"))
+-- for index, entry in ipairs(examples) do
+-- 	assert(test(entry[1], entry[2], index))
+-- end
 --
 -- print("Sucesso !!")
 
